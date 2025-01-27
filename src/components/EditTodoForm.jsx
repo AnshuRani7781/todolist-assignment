0066d5c4417285736508abcb5c7441cf89cf5744
Anshu Rani <anshu.rani@trustt.com>
2025-01-27 12:11:50 +0530

    enable and disable

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 import { SlCalender } from "react-icons/sl";
 import { useHeader } from "../HeaderContext";
 import { IoArrowBack } from "react-icons/io5";
-
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 const EditTodoForm = ({ todo, onCancel, onUpdate }) => {
   const [title, setTitle] = useState(todo.title);
   const [detail, setDetail] = useState(todo.detail);
@@ -101,24 +102,40 @@ const EditTodoForm = ({ todo, onCancel, onUpdate }) => {
           <button
             style={{
               flex: 1,
-
               padding: "20px",
               fontSize: "16px",
               fontWeight: "bold",
               color: "#fff",
-              backgroundColor: "#9395D3",
+              backgroundColor: title && detail && date ? "#9395D3" : "#d3d3d3", // Change color based on enabled/disabled
               border: "none",
               borderRadius: "15px",
-              cursor: "pointer",
+              cursor: title && detail && date ? "pointer" : "not-allowed", // Change cursor style
               transition: "background-color 0.3s",
               boxShadow: "0px 4px 4px 0px #00000040",
             }}
-            onMouseEnter={(e) => (e.target.style.backgroundColor = "#4c3bc0")}
-            onMouseLeave={(e) => (e.target.style.backgroundColor = "#9395D3")}
+            onMouseEnter={(e) =>
+              title && detail && date
+                ? (e.target.style.backgroundColor = "#4c3bc0")
+                : null
+            }
+            onMouseLeave={(e) =>
+              title && detail && date
+                ? (e.target.style.backgroundColor = "#9395D3")
+                : null
+            }
             onClick={handleSubmit}
+            disabled={!title || !detail || !date}
+            data-tooltip-id="enable"
+            data-tooltip-content={
+              !title || !detail || !date
+                ? "Please fill out all fields to enable the button."
+                : "Click to update the task"
+            }
+            // Disable button if any field is empty
           >
             Update
           </button>
+          <ReactTooltip id="enable" place="bottom" effect="solid" type="info" />
           <button
             style={{
               flex: 1,
